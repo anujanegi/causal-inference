2c6797fadcbe3081365f4c7ef5c561b940efffef
anujanegi <anujanegi1@gmail.com>
2021-07-06 12:26:45 +0200

    pplot utility for estimate stimulus position

--- plot.py
+++ plot.py
@@ -15,29 +15,60 @@ def plot_probability_varying_parameters(x_v, x_a, probability_function, xlabel='
 
     fig.add_subplot(221)
     
-    p_common_values = np.linspace(0.1, 0.8, 5)
+    p_common_values = np.linspace(0.1, 1, 5)
     for i, p_common in enumerate(p_common_values):
         plot_figure(x_v-x_a, probability_function(x_v, x_a, p_common=p_common), xlabel, ylabel, '$p_{commom}$ = %.2f'%p_common, 'Effect of $p_{common}$')
        
     fig.add_subplot(222)
 
-    sigma_v_values = np.linspace(0.1, 1.2, 5)
+    sigma_v_values = np.linspace(0.1, 5, 5)
     for i, sigma_v in enumerate(sigma_v_values):
         plot_figure(x_v-x_a, probability_function(x_v, x_a, sigma_v=sigma_v), xlabel, ylabel, '$\sigma_v$ = %.2f'%sigma_v, 'Effect of $\sigma_v$')
        
     fig.add_subplot(223)
    
-    sigma_a_values = np.linspace(0.1, 1.2, 5)
+    sigma_a_values = np.linspace(0.1, 5, 5)
     for i, sigma_a in enumerate(sigma_a_values):
         plot_figure(x_v-x_a, probability_function(x_v, x_a, sigma_a=sigma_a), xlabel, ylabel, '$\sigma_a$ = %.2f'%sigma_a, 'Effect of $\sigma_a$')
 
     fig.add_subplot(224)
     
-    sigma_p_values = np.linspace(1, 30, 5)
+    sigma_p_values = np.linspace(1, 15, 5)
     for i, sigma_p in enumerate(sigma_p_values):
         plot_figure(x_v-x_a, probability_function(x_v, x_a, sigma_p=sigma_p), xlabel, ylabel, '$\sigma_p$ = %.2f'%sigma_p, 'Effect of $\sigma_p$')
     
     plt.suptitle(title)
 
-def plot_estimate_stimulus_position():
-    pass
+def plot_estimate_stimulus_position(x_v, x_a, estimate_function, xlabel='', ylabel='', title=''):
+
+    fig = plt.figure(figsize=(20,10))
+
+    fig.add_subplot(221)
+    
+    p_common_values = np.linspace(0.1, 1, 5)
+    for i, p_common in enumerate(p_common_values):   
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='video', p_common=p_common), xlabel, ylabel, '$p_{commom}$ = %.2f'%p_common, 'Effect of $p_{common}$')
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='audio', p_common=p_common), xlabel, ylabel, '$p_{commom}$ = %.2f'%p_common, 'Effect of $p_{common}$', fmt='--')
+    
+    fig.add_subplot(222)
+
+    sigma_v_values = np.linspace(0.1, 5, 5)
+    for i, sigma_v in enumerate(sigma_v_values):
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='video', sigma_v=sigma_v), xlabel, ylabel, '$\sigma_v$ = %.2f'%sigma_v, 'Effect of $\sigma_v$')
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='audio', sigma_v=sigma_v), xlabel, ylabel, '$\sigma_v$ = %.2f'%sigma_v, 'Effect of $\sigma_v$', fmt='--')
+       
+    fig.add_subplot(223)
+   
+    sigma_a_values = np.linspace(0.1, 5, 5)
+    for i, sigma_a in enumerate(sigma_a_values):
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='video', sigma_a=sigma_a), xlabel, ylabel, '$\sigma_a$ = %.2f'%sigma_a, 'Effect of $\sigma_a$')
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='audio', sigma_a=sigma_a), xlabel, ylabel, '$\sigma_a$ = %.2f'%sigma_a, 'Effect of $\sigma_a$', fmt='--')
+
+    fig.add_subplot(224)
+    
+    sigma_p_values = np.linspace(1, 15, 5)
+    for i, sigma_p in enumerate(sigma_p_values):
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='video', sigma_p=sigma_p), xlabel, ylabel, '$\sigma_p$ = %.2f'%sigma_p, 'Effect of $\sigma_p$')
+        plot_figure(x_v-x_a, estimate_function(x_v, x_a, type='audio', sigma_p=sigma_p), xlabel, ylabel, '$\sigma_p$ = %.2f'%sigma_p, 'Effect of $\sigma_p$', fmt='--')
+    
+    plt.suptitle(title)
